a15597711d587fe103f18db0fc75d7a67ac402b9
Vincent Ramirez <vincent.ramirez@frontlinebusiness.com.ph>
2026-05-19 16:04:21 +0800

    modified base vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/viter-sms/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/viter-sms/" : "/",
   plugins: [react(), tailwindcss()],
-});
+}));
